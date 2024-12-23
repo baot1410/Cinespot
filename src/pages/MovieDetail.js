4af9ca770ec457b9ad6da1d853a3d81c2bfae772
baot1410 <baot1410@uw.edu>
2024-12-22 17:41:19 -0800

    final #2 fixed esLinter

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -75,7 +75,7 @@ const MovieDetail = () => {
 
         <p className = "my-4">
           <span className="mr-2 font-bold">IMDB Code:</span>
-          <a href={`https://www.imdb.com/title/${curMovie.imdb_id}`} target="_blank" rel="noreferer">{curMovie.imdb_id}</a>
+          <a href={`https://www.imdb.com/title/${curMovie.imdb_id}`} target="_blank" rel="noopener noreferrer">{curMovie.imdb_id}</a>
         </p>
 
         </div>
